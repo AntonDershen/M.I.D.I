a3d4c3f6635e416385b30aa867de660220b5abba
Anton Dershen <dershen95@gmail.com>
2017-01-29 14:30:36 +0300

    Добавлен HotPlug Arduino-устройств

--- M.I.D.I/M.I.D.I/MainPage.xaml.cs
+++ M.I.D.I/M.I.D.I/MainPage.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using Windows.UI.Xaml.Controls;
 using M.I.D.I.Infrastructure;
 using M.I.D.I.DataModels;
+using Windows.Devices.SerialCommunication;
+using Windows.ApplicationModel.Core;
+
 namespace M.I.D.I
 {
     public sealed partial class MainPage : Page
     {
         DependencyResolver dependencyResolver;
+        Windows.UI.Core.CoreDispatcher dispatcher;
+
         public MainPage()
         {
+            dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
             this.InitializeComponent();
+            this.InitializeDevices();
             dependencyResolver = new DependencyResolver();
         }
+
     }
 }
