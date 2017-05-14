048a5d2619f237b9b9ea29d535a80812ee0f89fa
Anton Dershen <dershen95@gmail.com>
2017-05-14 20:18:40 +0300

    Изменен пользовательский интерфейс

---
+++ M.I.D.I/BussinessLogic/ConvertService/ConvertMp3.cs
@@ -0,0 +1,21 @@
+﻿using BussinessLogic.Interface.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessLogic.Interface.Entities;
+using System.IO;
+
+namespace BussinessLogic.ConvertService
+{
+    public class ConvertMp3 : IConvertService
+    {
+        public IEnumerable<NoteEntity> GetNotes()
+        {
+            ConvertWav convertWav = new ConvertWav();
+            var notes = convertWav.GetNotes();
+            return notes;
+        }
+    }
+}
