30790870a08062a70ba03afee942f00df6ab4480
Anton Dershen <dershen95@gmail.com>
2017-05-14 14:22:37 +0300

    Добавлены классы для Wave конвертации

---
+++ M.I.D.I/BussinessLogic/ConvertService/ConvertWav.cs
@@ -0,0 +1,22 @@
+﻿using BussinessLogic.Interface.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessLogic.Interface.Entities;
+
+
+namespace BussinessLogic.ConvertService
+{
+    public class ConvertWav : IConvertService
+    {
+        public IEnumerable<NoteEntity> GetNotes()
+        {
+            List<byte> data = new List<byte>();
+            WaveFile waveFile = new WaveFile("e:\\march.wav");
+            waveFile.Read();
+            return null;
+        }
+    }
+}
